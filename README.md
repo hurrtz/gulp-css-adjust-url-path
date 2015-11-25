c0c3647e16031c4c52344cbc5018cb97199b0919
Tobias Winkler <tobias.winkler@useeds.de>
2015-11-25 16:07:40 +0100

    initial commit
    Signed-off-by: Tobias Winkler <tobias.winkler@useeds.de>

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gulp-css-adjust-url-path
-Adjust absolute CSS url-properties so that they become relative upon build.
+
+Correct the url-property inside CSS to make absolute paths relative, no matter where the CSS files are located.
+
+## Install
+
+```
+$ npm install --save-dev gulp-css-adjust-url-path
+```
+
+
+## Usage
+
+
+## License
+
+MIT © [Tobias Winkler](http://tobiaswinkler.berlin)
---
+++ index.js
@@ -0,0 +1,54 @@
+var through = require('through2'),
+    replace = require('gulp-replace'),
+    gutil = require('gulp-util'),
+    PluginError = gutil.PluginError;
+
+// Consts
+const PLUGIN_NAME = 'gulp-css-adjust-path';
+
+function handleStream() {
+    var stream = through();
+
+    return stream;
+}
+
+// Plugin level function(dealing with files)
+function gulpCssAdjustPath(needle) {
+    var pathArray,
+        replacement;
+
+    if (!needle) {
+        throw new PluginError(PLUGIN_NAME, 'Missing search value!');
+    }
+
+    if (!needle instanceof RegExp) {
+        throw new PluginError(PLUGIN_NAME, 'Search value must be a regex containing two capturing groups!');
+    }
+
+    // Creating a stream through which each file will pass
+    return through.obj(function (file, enc, cb) {
+        pathArray = file.path.substring(file.base.length).split('/');
+        replacement = '$1' + '../' + Array(pathArray.length).join('../') + '$2';
+
+        if (file.isNull()) {
+            // return empty file
+            return cb(null, file);
+        }
+
+        if (!pathArray.length) {
+            // replace absolute path with: look on same level
+            replacement = '$1./$2';
+        }
+
+        if (file.isStream()) {
+            file.contents = file.contents.pipe(replace(needle, replacement));
+        } else if (file.isBuffer()) {
+            file.contents = new Buffer(String(file.contents).replace(needle, replacement));
+        }
+
+        return cb(null, file);
+    });
+}
+
+// Exporting the plugin main function
+module.exports = gulpCssAdjustPath;
